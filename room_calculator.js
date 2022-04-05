db18fe8a624f64fc5b83976029910ff027c463f9
Bri Codes <briannajhowell@gmail.com>
2022-04-05 10:13:22 -0400

    updated features for room calculator

--- room_calculator.js
+++ room_calculator.js
@@ -1,3 +1,17 @@
+// DESCRIPTION
+// This code calculates the area of a room in square feet.
+
+// PROBLEMS
+// Code accepts non-numeric inputs and calculates as NaN.
+
+// SOLUTIONS
+// Refactor code to throw errors when non-numeric inputs are entered.
+// Issue is within the invalidNumber function.
+// The issue is specific to the .trim() method within invalidNumber function.
+// The .trim() method is throwing a TypeError because .trim() is for strings.
+// See tip_calculator.js, loan_calculator.js, or rps_lizard_spock.js
+// to see the invalidNumber function working properly in those contexts.
+
 
 let readline = require('readline-sync');
 
@@ -5,11 +19,10 @@ function prompt(message) {
   console.log(`${message}`);
 }
 
-function invalidNumber(number) {
-  return number.trim() === '' ||
-  Number.isNaN(Number(number));
-}
-
+//function invalidNumber(input) {
+//return input.trim() === '' ||
+//Number.isNaN(Number(input));
+//}
 console.clear();
 
 prompt("Welcome to the Room Calculator!");
@@ -18,33 +31,38 @@ while (true) {
 
   const METERS_TO_SQFEET = 10.7639;
 
-  let length = parseFloat(readline.question('Step 1: Enter the length of the room in meters:\n'));
+  prompt("Step 1: Enter the length of the room in meters:");
+  let length = parseFloat(readline.question());
 
-  while (invalidNumber(length)) {
-    prompt("Please enter a valid number.");
-    length = readline.question();
-  }
-  let width = parseFloat(readline.question('Step 2: Enter the width of the room in meters:\n'));
+  //while (invalidNumber(length)) {
+  //prompt("Please enter a valid number.");
+  //length = readline.question();
+  //}
 
-  while (invalidNumber(width)) {
-    prompt("Please enter a valid number.");
-    width = readline.question();
+  prompt("Step 2: Enter the width of the room in meters:");
+  let width = parseFloat(readline.question());
 
-    let area = length * width;
-    let areaSqFeet = area * METERS_TO_SQFEET;
+  //while (invalidNumber(width)) {
+  //prompt("Please enter a valid number.");
+  //width = readline.question();
 
-    console.log(`The area of the room is ${area.toFixed(2)} square meters (${areaSqFeet.toFixed(2)} square feet)`);
+  let area = length * width;
+  let areaSqFeet = area * METERS_TO_SQFEET;
 
-    prompt("Would you like to calculate another room?");
-    let answer = readline.question();
+  console.log(`The area of the room is ${area.toFixed(2)} square meters (${areaSqFeet.toFixed(2)} square feet)`);
 
-    if (answer[0].toLowerCase() === 'y') {
-      console.clear();
-      continue;
-    } else if (answer[0].toLowerCase() === 'n') {
-      console.log("Thank you for using the Room Calculator!");
-      console.log("\nCreated by Brianna Howell, in February 2022.\nModified on April 1, 2022");
-      break;
-    }
+  prompt("\nWould you like to calculate another room?");
+  prompt("Answer: ex. 'yes' or 'no'");
+  let answer = readline.question();
+
+  if (answer[0].toLowerCase() === 'y') {
+    console.clear();
+    continue;
+  } else if (answer[0].toLowerCase() === 'n') {
+    console.clear();
+    console.log("\nThank you for using the Room Calculator!");
+    console.log("\nCreated by Brianna Howell, in February 2022.\nModified on April 1, 2022\n");
+    break;
   }
 }
+
